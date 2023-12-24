bdbfb5aba3b505630de4297065ea945dc0c6764c
umarkasenov <casenov2101@icloud.com>
2023-12-24 11:20:48 +0600

    hw

---
+++ class1.py
@@ -0,0 +1,7 @@
+class Hello:
+    def greet(self):
+        return "Hello, World!"
+
+class NewHello(Hello):
+    def tptint(self):
+        return "This is a new function!"
--- task.py
+++ task.py
@@ -1,3 +1,10 @@
 from art import tprint
+from class1 import NewHello
 tprint("PYTHON")
 
+new_hello_instance = NewHello()
+greeting = new_hello_instance.greet()
+tptint_result = new_hello_instance.tptint()
+
+print(greeting)
+print(tptint_result)
